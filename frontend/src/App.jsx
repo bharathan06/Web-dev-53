fcc3bb2f5b58fa731ddabf5981112209cc3ed0bb
Bharathan <bharathan.m.blr@gmail.com>
2023-12-05 23:42:37 +0530

    Created navbar, created routes for different pages bharathan is working on

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import { Navbar } from "./components/navbar"
+import { Navbar } from "./components/navbar/navbar"
+import { Home } from "./pages/home/home"
+import { About } from "./pages/about/about"
+
+
 function App() {
 
   return (
-    <nav>
-      <Navbar />
-    </nav>
     
-    // <Router>
-      
-    //   <Routes>
-    //     <Route path="/" />
-    //     <Route path="/cart" />
+    
+    <Router>
+      <Navbar />
+      <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/about" element={<About/>}/>
+      {/* <Route path="/cart" element={<Cart/>}/> */}
 
-    //   </Routes>
-    // </Router>
+
+      </Routes>
+    </Router>
     
   );
 }
