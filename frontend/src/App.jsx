332df99e21687f210333c1f939bb36251b63cc41
Ananya Bhat <135028764+anyaaa1924@users.noreply.github.com>
2023-12-09 12:48:26 +0530

    profile
    account sidebar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,25 +3,30 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Navbar } from "./components/navbar/navbar"
 import { Home } from "./pages/home/home"
 import { About } from "./pages/about/about"
+import { Profile } from "./pages/Account/profile" // <-- change the casing of the import statement
 
 
 function App() {
 
   return (
-    
+    <div className="App">
+      <profile />
+    </div>
+  )
+};   
     
     <Router>
       <Navbar />
       <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/about" element={<About/>}/>
+      <Route path="/profile" element={<Profile/>}/>
       {/* <Route path="/cart" element={<Cart/>}/> */}
 
 
       </Routes>
     </Router>
     
-  );
-}
+  
 
 export default App
